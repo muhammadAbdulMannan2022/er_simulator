9bfc327c42a8e85ed6c2d990f22aa57646409ffb
Samin Yasar <yasarsamin57@gmail.com>
2025-11-05 12:11:05 +0600

    fix: safearea wrapper removed from the reusable layout component

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,10 +2,10 @@ import { SafeAreaView, ScrollView, StatusBar } from 'react-native';
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
-    <SafeAreaView className="flex-1">
-      <ScrollView className="flex-1" contentContainerStyle={{ paddingBottom: 20 }}>
-        {children}
-      </ScrollView>
-    </SafeAreaView>
+    // <SafeAreaView className="flex-1">
+    <ScrollView className="flex-1 bg-[#E5E5E5]" contentContainerStyle={{ paddingBottom: 20 }}>
+      {children}
+    </ScrollView>
+    // </SafeAreaView>
   );
 }
